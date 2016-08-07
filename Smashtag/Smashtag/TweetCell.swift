5d24da8dea058ae4f16b4b372113326d5c45e122
Katja Hollaar <afkatja@gmail.com>
2016-08-07 21:58:40 +0200

    Smashtag - added highlighting for tweet hashtags and user mentions

--- Smashtag/Smashtag/TweetCell.swift
+++ Smashtag/Smashtag/TweetCell.swift
@@ -33,8 +33,21 @@ class TweetCell: UITableViewCell {
     TweetCreated?.text = nil
 
     if let tweet = self.tweet {
-      TweetText?.text = tweet.text
+
       UserName?.text = "\(tweet.user)"
+
+      var tweetText = NSMutableAttributedString(string: tweet.text)
+      for mention in tweet.userMentions {
+        tweetText.addAttribute(NSForegroundColorAttributeName, value: UIColor.blueColor(), range: mention.nsrange)
+        tweetText.addAttributes([NSForegroundColorAttributeName: UIColor.blueColor(), NSFontAttributeName: UIFont.boldSystemFontOfSize(18.0)], range: mention.nsrange)
+      }
+      for hashtag in tweet.hashtags {
+        tweetText.addAttribute(NSForegroundColorAttributeName, value: UIColor.blueColor(), range: hashtag.nsrange)
+        tweetText.addAttributes([NSForegroundColorAttributeName: UIColor.blueColor(), NSFontAttributeName: UIFont.boldSystemFontOfSize(18.0)], range: hashtag.nsrange)
+      }
+
+      TweetText?.attributedText = tweetText
+
       if let profileImage = tweet.user.profileImageURL {
         //TODO use dispatch async
         if let imageData = NSData(contentsOfURL: profileImage) {
@@ -43,6 +56,13 @@ class TweetCell: UITableViewCell {
       }
       TweetCreated?.text = "\(tweet.created)"
     }
-
   }
 }
+
+//private extension NSMutableAttributedString {
+//  func addAttributes(attrs: [String : AnyObject], mentions: [Tweet.userMentions]) {
+//    for keyword in mentions {
+//      self.addAttributes(attrs, range: keyword.nsrange)
+//    }
+//  }
+//}
